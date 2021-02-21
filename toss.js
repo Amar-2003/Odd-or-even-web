0c2f88929607855ec4c6d615d1ad29a9b5591662
Amarnath K <amarnathk5674@gmail.com>
2021-02-21 21:52:32 +0530

    Made till random for toss

--- toss.js
+++ toss.js
@@ -5,12 +5,20 @@ document.addEventListener("DOMContentLoaded",function (){
         odd.style.visibility = "hidden"
         even.style.backgroundColor = "green"
         document.querySelector('#feedback1').innerHTML = "You chose even!"
-        let chosen = "odd"
+        let chosen = 'o'
     })
     odd.addEventListener("click", function (){
         even.style.visibility = "hidden"
         odd.style.backgroundColor = "green"
         document.querySelector('#feedback1').innerHTML = "You chose odd!"
-        let chosen = "even"
+        let chosen = 'e'
     })
+    let button = document.querySelector('#tosssubmit')
+    button.addEventListener('click',function (){
+        let input = parseInt(document.querySelector('#input').value);
+        let cmt = Math.floor(Math.random() + 1.5);
+        document.querySelector('#feedback2').innerHTML = "Computer chose " + cmt;     
+
+    })
+
 })
