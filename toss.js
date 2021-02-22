02032a6909a3ec7cca14eee1c803ca015dee2a6b
Amarnath K <amarnathk5674@gmail.com>
2021-02-22 10:35:43 +0530

    issues with var chosen

--- toss.js
+++ toss.js
@@ -5,20 +5,35 @@ document.addEventListener("DOMContentLoaded",function (){
         odd.style.visibility = "hidden"
         even.style.backgroundColor = "green"
         document.querySelector('#feedback1').innerHTML = "You chose even!"
-        let chosen = 'o'
+        let chosen = "e"
     })
     odd.addEventListener("click", function (){
         even.style.visibility = "hidden"
         odd.style.backgroundColor = "green"
         document.querySelector('#feedback1').innerHTML = "You chose odd!"
-        let chosen = 'e'
+        let chosen = "o"
     })
+    let batbutton = document.querySelector('#batchoice')
+    let ballbutton = document.querySelector('#bowlchoice')
+    batbutton.style.visibility = "hidden"
+    ballbutton.style.visibility = "hidden"
+    let tossquestion = document.querySelector('#tossquestion')
+    tossquestion.style.visibility = "hidden"
+
     let button = document.querySelector('#tosssubmit')
     button.addEventListener('click',function (){
         let input = parseInt(document.querySelector('#input').value);
         let cmt = Math.floor(Math.random() + 1.5);
-        document.querySelector('#feedback2').innerHTML = "Computer chose " + cmt;     
-
+        document.querySelector('#feedback2').innerHTML = "Computer chose " + cmt;
+        if("e".localeCompare(chosen))
+        {
+            if((cmt+input)%2 == 0)
+            {
+                document.querySelector('#feedback3').innerHTML = "You won the toss";
+                tossquestion.style.visibility = "visible"
+            }
+        }
+        
     })
 
 })
