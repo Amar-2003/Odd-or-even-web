00327a69496fcd594d37195acd7ce59e03f5ce9f
Amarnath K <amarnathk5674@gmail.com>
2021-02-23 18:39:55 +0530

    bug fixes,still bugs

--- toss.js
+++ toss.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded",function (){
     let startplay1 = document.querySelector('#start_play1')
     startplay1.style.visibility = "hidden"
@@ -125,38 +126,12 @@ document.addEventListener("DOMContentLoaded",function (){
                 }
 
             }
+            function userbatting(){
+                return userbat;
+            }
         }
         
     })
     
-    //play 1
-    let score1 = 0
-    let score2 = 0
+    })
     
-    while(true)
-    {
-        document.querySelector('#runinput1_button').addEventListener('click',function (){
-            let userrun = parseInt(document.querySelector('#runinput1').value);
-            let cmprun = Math.floor(Math.random() * 7);
-            document.querySelector('#enteredrun_1').innerHTML = "You entered " + userrun;
-            document.querySelector('#compchosenrun').innerHTML = "Computer chose " + cmprun;
-            
-            if(userbat)
-            {   
-                document.querySelector('#status').innerHTML = "You are batting"
-                if(userrun == 2)
-                {
-                    document.querySelector('#feedback4'),innerHTML = "you are out :("
-                }
-                else
-                {
-                    score1 += userrun
-                    document.querySelector('#player runs').innerHTML = "Your current run is " + score1
-                }
-            }
-
-
-        })
-    }
-
-})
